7ccf511a7870308a272b659208f8558118d3b6a3
Lucas <lucasrolim@poli.ufrj.br>
2016-12-11 00:25:42 -0200

    criando arquivos

--- gradient.cpp
+++ gradient.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+double gradiente_f1(double x, double y,int variavel){
+    double gradiente[2];
+    gradiente[0] = -1*(x*(1-y)-2*x*y*(1-y))/(x*(1-x)*y*(1-y));
+    gradiente[1] = -1*(y*(1-x)-2*y*x*(1-x))/(x*(1-x)*y*(1-y));
+    return gradiente[variavel];
+}
+
 int main()
 {
- cout << "Sou a função do gradiente! Abstraia" << endl;
- return 0;
+ cout << gradiente_f1(13,20,1) << endl;
+
+ //  Aplicação do método do gradiente para F1
+ 0;
 }
